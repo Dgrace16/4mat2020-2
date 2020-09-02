237499f7acd17ed750291ecce227621a4828cbd6
Dgrace16 <dgracecenteno@gmail.com>
2020-09-02 14:12:58 +0000

    (02/09) Primeiro model

---
+++ back-end/controllers/curso.js
@@ -0,0 +1,26 @@
+/*
+    QUATRO OPERÇÕES BÁSICAS SOBRE DADOS
+    1) CREATE (criação ou inserção)
+        Cria um novo objetodentro da coleção
+
+    2) RETRIEVE (recuperação ou listagem)
+        Permite recuperar os objetos a partir do BANCO de DADOS
+    
+    3) UPDATE (atualização)
+        Altera os valores de um objeto que JÁ EXISTE no BANCO de DADOS
+    
+    4) DELETE (exclusão)
+        Elimina um objeto do BANCO de DADOS
+        
+    FAMOSO CRUD = (C)REATE + (R)ETRIEVE + (U)PDATE + (D)ELETE
+
+    VERBOS ASSOCIADOS HTTP ÁS OPERAÇÕES CRUD
+    
+    VERBO       Operação      
+    
+    POST        Creat
+    GET         Retrieve
+    PUT         Update
+    DELETE      Delete
+
+*/
---
+++ back-end/models/Curso.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({ // Schema é um esquema 
+    nome: {
+        type: String, // tipo String e sem limitação 
+        required: true // atibuto obrigatorio
+    },
+    carga_horaria: {
+        type: Number, // tipo Number tanto integer como Float 
+        required: true, // atibuto obrigatorio
+        min: 4, // minimo de horas
+        max: 240, // maximo de horas 
+        default: 100 // caso eu nn Coloque valor como padrão será 100
+    },
+    nivel: {
+        type: String, // tipo String e sem limitação 
+        required: true, // atibuto obrigatorio
+        enum: ['Básico', 'Intermediário', 'Avançado'] // Conjunto de valores validos
+    },
+    valor_curso: {
+        type: Number, // tipo Number tanto integer como Float
+        required: true, // atibuto obrigatorio
+        min: 50 // minimo do valor
+    }
+})
+
+// PARÂMETROS DO mongoose.model()
+// 1º -> Nome do model (inicial maiúscula, igual ao nome do arquivo)
+// 2º -> a constante esquema, montada anteriormente
+// 3º -> o nome da COLEÇÃO no BD que irá receber os objetos que serão
+//       criados a partir deste model (inicial minúscula, plural do
+//       nome do model)
+module.exports = mongoose.model('Curso', esquema, 'cursos')
