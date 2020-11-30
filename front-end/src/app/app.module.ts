9c5a8dfc734394d984926bdf0254ed6c2b2af00a
Dgrace16 <dgracecenteno@gmail.com>
2020-11-30 22:09:40 +0000

    (30/11) Correção parcial de erros (peça esta com erros na compilação) .

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -27,15 +27,17 @@ import { CarroListComponent } from './carro/carro-list/carro-list.component';
 import { ClienteListComponent } from './cliente/cliente-list/cliente-list.component';
 import { FuncionarioListComponent } from './funcionario/funcionario-list/funcionario-list.component';
 import { ManutencaoListComponent } from './manutencao/manutencao-list/manutencao-list.component';
-import { PecaListComponent } from './peca/peca-list/peca-list.component';
+
 import { ServicoListComponent } from './servico/servico-list/servico-list.component';
 
 import { CarroFormComponent } from './carro/carro-form/carro-form.component';
 import { ClienteFormComponent } from './cliente/cliente-form/cliente-form.component';
 import { FuncionarioFormComponent } from './funcionario/funcionario-form/funcionario-form.component';
 import { ManutencaoFormComponent } from './manutencao/manutencao-form/manutencao-form.component';
-import { PecaFormComponent } from './peca/peca-form/peca-form.component';
+
 import { ServicoFormComponent } from './servico/servico-form/servico-form.component';
+import { PecaListComponent } from './peca/peca-list/peca-list.component';
+import { PecaFormComponent } from './peca/peca-form/peca-form.component';
 
 @NgModule({
   declarations: [
--- front-end/src/app/peca/peca-form/peca-form.component.ts
+++ front-end/src/app/peca/peca-form/peca-form.component.ts
@@ -1,82 +1,81 @@
 import { PecaService } from './../peca.service';
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Location } from '@angular/common';
 
 @Component({
-    selector: 'app-peca-form',
-    templateUrl: './peca-form.component.html',
-    styleUrls: ['./peca-form.component.scss']
+  selector: 'app-peca-form',
+  templateUrl: './peca-form.component.html',
+  styleUrls: ['./peca-form.component.scss']
 })
 export class PecaFormComponent implements OnInit {
 
-    title: string = 'novo peca'
+    title : string = 'Nova peça'
 
     peca: any = {} //Objeto vazio, nome da entidade no SINGULAR
 
-    constructor(
-        private pecaSrv: PecaService,
-        private snackBar: MatSnackBar,
-        private location: Location,
-        private actRoute: ActivatedRoute
-    ) { }
+  constructor(
+    private pecaSrv: PecaService,
+    private snackBar: MatSnackBar,
+    private location: Location,
+    private actRoute: ActivatedRoute
+  ) { }
 
- async ngOnInit() {
-        // Verificando se existe id na rota que trouxe ao formulário
-        if (this.actRoute.snapshot.params['id']) {
-            try {
-                // 1) Trazer o registro do back-end para edição
-                this.peca = await this.pecaSrv.obterUm(this.actRoute.snapshot.params['id'])
-                // 2) Mudar o título da página
-                this.title = 'Editando peca'
-            }
-            catch (erro) {
-                console.log(erro)
-                this.snackBar.open('ERRO: não foi possível carregar os dados para edição.',
-                    'Que pena! Alguem vacilo!', { duration: 5000 })
-            }
-        }
-
-
+  async ngOnInit() {
+    // Verificando se existe id na rota que trouxe ao formulário
+    if(this.actRoute.snapshot.params['id']) {
+      try {
+        // 1) Trazer o registro do back-end para edição
+        this.peca = await this.pecaSrv.obterUm(this.actRoute.snapshot.params['id'])
+        // 2) Mudar o título da página
+        this.title = 'Editando peça'
+      }
+      catch(erro) {
+        console.log(erro)
+        this.snackBar.open('ERRO: não foi possível carregar os dados para edição.',
+          'Que pena! Alguem vacilo!', { duration: 5000 })
+      }
     }
-    async salvar(form: NgForm) {
-        try {
-            if (form.valid) {
-                // 1) Enviar os dados para o back-end para serem salvos
-                if (this.peca._id) {
-                    // _id existe, esse registro já foi salvo anteriormente
-                    // no BD e é caso de atualização
-                    await this.pecaSrv.atualizar(this.peca)
-                }
-                else {
-                    await this.pecaSrv.novo(this.peca)
-                }
-                // 2) Dar um feedback (mensagem) para o usuário
-                this.snackBar.open('Dados salvos com sucesso.', 'Entendi',
-                    { duration: 5000 })
-                // 3) Voltar para a tela de listagem
-                this.location.back()
-            }
-        }
-        catch (erro) {
-            console.log(erro)
-            this.snackBar.open('ERRO: não foi possível salvar os dados.', 'Que pena! DERAM MOLE!',
-                { duration: 5000 })
-        }
-    }
+   }
 
-    voltar(form: NgForm) {
-        let result = true
-        // form.dirty = formulário "sujo", não salvo (via código)
-        // form.touched = o conteúdo de algum campo foi alterado (via usuário)
-        if (form.dirty && form.touched) {
-            result = confirm('Há dados não salvos. Deseja realmente voltar?')
+    async salvar(form : NgForm) {
+    try {
+      if(form.valid) {
+        // 1) Enviar os dados para o back-end para serem salvos
+        if(this.peca._id) {
+          // _id existe, esse registro já foi salvo anteriormente
+          // no BD e é caso de atualização
+          await this.pecaSrv.atualizar(this.peca)
         }
-        // Retorna à página anterior se resposta foi positiva ou se o formulário
-        // estiver "limpo"
-        if (result) this.location.back()
+        else {
+          await this.pecaSrv.novo(this.peca)
+        }
+        // 2) Dar um feedback (mensagem) para o usuário
+        this.snackBar.open('Dados salvos com sucesso.', 'Entendi',
+          { duration: 5000 })
+        // 3) Voltar para a tela de listagem
+        this.location.back()
+      }
     }
+    catch(erro) {
+      console.log(erro)
+      this.snackBar.open('ERRO: não foi possível salvar os dados.', 'Que pena! DERAM MOLE!',
+        { duration: 5000 })
+    }
+  }
+
+  voltar(form : NgForm) {
+    let result = true
+    // form.dirty = formulário "sujo", não salvo (via código)
+    // form.touched = o conteúdo de algum campo foi alterado (via usuário)
+    if(form.dirty && form.touched) {
+      result = confirm('Há dados não salvos. Deseja realmente voltar?')
+    }
+    // Retorna à página anterior se resposta foi positiva ou se o formulário
+    // estiver "limpo"
+    if(result) this.location.back()
+  }
 
 }
